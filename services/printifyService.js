e02506e571e95668e25f4a6e82c80c55bdcd9cd8
Jamal <hostheavens@gmail.com>
2025-06-24 09:08:32 +0200

    Update printifyService.js

--- services/printifyService.js
+++ services/printifyService.js
@@ -131,7 +131,7 @@ export async function createProduct({
 
 export async function createTestProduct() {
   return createProduct({
-    imageUrl: 'https://res.cloudinary.com/dpe19wze8/image/upload/f_auto,q_auto/v1750685860/crosswords/fxwnicjzetg3xmeo9xuy.png',
+    imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/7/77/Delete_key1.jpg',
   });
 }
 
