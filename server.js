c36ba5535139c652d79b9c6b43296c1a270ecc33
Jamal <hostheavens@gmail.com>
2025-07-07 18:09:47 +0200

    Update server.js

--- server.js
+++ server.js
@@ -22,7 +22,8 @@ try {
 }
 
 const { createOrder } = printifyService;
-
+const app = express();
+app.use(cors(corsOptions));
 // Shopify raw body middleware for HMAC verification
 app.use('/webhooks/orders/create', bodyParser.raw({ type: 'application/json', limit: '2mb' }));
 
@@ -122,8 +123,6 @@ const corsOptions = {
   credentials: true,
 };
 
-const app = express();
-app.use(cors(corsOptions));
 
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
