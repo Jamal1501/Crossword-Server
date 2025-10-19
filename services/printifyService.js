9f59d0a060a24b53a4f9ab185ad322f80aa83dba
Jamal <hostheavens@gmail.com>
2025-10-19 23:32:06 +0200

    Adjust finalScale default value to 0.6

--- services/printifyService.js
+++ services/printifyService.js
@@ -377,7 +377,7 @@ export async function createOrder({
   }
 
   // 4) Contain-fit scale for FRONT (avoid clipping), using your contain helper
-  let finalScale = position?.scale ?? 1;
+  let finalScale = position?.scale ?? 0.6;
   try {
     const ph = await getVariantPlaceholder(blueprintId, printProviderId, parseInt(variantId));
     finalScale = clampContainScale({
